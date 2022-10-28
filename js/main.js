dbab253cafddf9a579638f55767a368b44b74589
Jefersson Velasquez <jeferssonveme.97@gmail.com>
2022-10-27 19:16:05 -0500

    fusion del menu desktop, fusion del menu mobile y carrito de compras

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu')
+const iconMenu = document.querySelector('.menu');
+const mobilMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const menuProductDetail = document.querySelector('.product-detail')
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+iconMenu.addEventListener('click', toggleIconMenu);
+menuCarritoIcon.addEventListener('click', toggleProductDetail);
+
+
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function  toggleIconMenu(){
+    mobilMenu.classList.toggle('inactive');
+}
+function toggleProductDetail(){
+    menuProductDetail.classList.toggle('inactive');
 }
